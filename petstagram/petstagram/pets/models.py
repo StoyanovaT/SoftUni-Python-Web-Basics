a2f4de8873085d98fc89cf4e13dc19590cad1f13
Tanya Styanova <stoyanovatanya17@gmail.com>
2022-11-17 14:52:29 +0200

    Some changes to the code

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,3 +1,17 @@
+# pets/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    MAX_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_NAME,
+        null=False,
+        blank=False,
+    )
+
+    personal_photo = models.URLField(
+        
+    )
